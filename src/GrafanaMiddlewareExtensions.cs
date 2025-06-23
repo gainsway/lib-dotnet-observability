581b38093785cdf1c8097e689b72805540bc5041
Daniele Iasella <2861984+overbit@users.noreply.github.com>
2025-06-23 09:31:35 +0100

    docs: add grafana middleware description

--- src/GrafanaMiddlewareExtensions.cs
+++ src/GrafanaMiddlewareExtensions.cs
@@ -4,6 +4,13 @@ namespace Gainsway.Observability;
 
 public static class GrafanaMiddlewareExtensions
 {
+    /// <summary>
+    /// This middleware adds a "server-timing" header to the response,
+    /// which is useful for monitoring and tracing in Grafana.
+    /// It's manadatory to integrate frontend and backend tracing in Grafana.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
     public static IApplicationBuilder UseGrafanaMiddleware(this IApplicationBuilder app) =>
         app.UseMiddleware<GrafanaMiddleware>();
 }
